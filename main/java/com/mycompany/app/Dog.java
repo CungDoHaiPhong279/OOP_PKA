d18426b2e295aec1f78fd52932e496fba0b9cd2e
Cung Do Hai Phong <23010341@st.phenikaa-uni.edu.vn>
2024-09-02 00:50:50 +0700

    added App/java

---
+++ main/java/com/mycompany/app/Dog.java
@@ -0,0 +1,27 @@
+public class Dog {
+    public void bark() {
+        System.out.println("toi xau trai");
+    }
+
+    public void bark(double height) {
+        System.out.println("A cao hon B " + height + " cm");
+    }
+
+    public void bark(int times) {
+        for (int i = 0; i < times; i++) {
+            bark();
+        }
+    }
+
+    public void bark(String sound) {
+        System.out.println(sound);
+    }
+
+    public void bark(int times, double height) {
+        for (int i = 0; i < times; i++) {
+            System.out.print("Woof! ");
+        }
+        System.out.println("Woof! Woof! at volume " + height);
+    }
+
+}
