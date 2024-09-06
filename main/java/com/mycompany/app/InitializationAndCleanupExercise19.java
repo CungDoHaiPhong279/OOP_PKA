193df47560a589db14fdc2ed406d4a4e0f711741
Cung Do Hai Phong <23010341@st.phenikaa-uni.edu.vn>
2024-09-06 10:21:50 +0700

    added Tank.java

---
+++ main/java/com/mycompany/app/InitializationAndCleanupExercise19.java
@@ -0,0 +1,15 @@
+public class InitializationAndCleanupExercise19 {
+    public static void printStrings(String... strings) {
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+    public static void main(String[] args) {
+        printStrings("apple", "banana", "cherry");
+
+        String[] fruits = {"kiwi", "mango", "pineapple"};
+        printStrings(fruits);
+    }
+
+}
+//giống spreach trong js 
